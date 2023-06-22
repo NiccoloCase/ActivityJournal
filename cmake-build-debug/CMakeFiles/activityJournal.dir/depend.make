69b5717843a9a260c091e3eab1318bbcd76eef1d
niccolocaselli <caselli.niccolo.developer@gmail.com>
2023-06-22 12:09:53 +0200

    Git

--- cmake-build-debug/CMakeFiles/activityJournal.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for activityJournal.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CMakeFiles/activityJournal.dir/main.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/activityJournal.dir/main.cpp.o: \
-  /Users/niccolocaselli/Desktop/uni/Laboratorio\ di\ programmazione/activityJournal/main.cpp
